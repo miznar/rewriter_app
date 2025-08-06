4afeadf85d302eb5a9dbfd41d5538148b40be9fd
Muhammad Azib Naeem <azibnaeem17official@gmail.com>
2025-08-07 00:13:41 +0500

    Onboarding

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
+import 'package:shared_preferences/shared_preferences.dart';
+import 'features/homescreen/home_screen.dart';
+import 'features/onboarding/onboarding_screen.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -8,36 +10,36 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+
+  final prefs = await SharedPreferences.getInstance();
+  final hasSeenOnboarding = prefs.getBool('hasSeenOnboarding') ?? false;
+
+  runApp(MyApp(hasSeenOnboarding: hasSeenOnboarding));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final bool hasSeenOnboarding;
+  const MyApp({super.key, required this.hasSeenOnboarding});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Rewriter',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
-      home: const MyHomePage(),
+      initialRoute:
+      hasSeenOnboarding ? AppRoutes.home : AppRoutes.onboarding,
+      routes: {
+        AppRoutes.onboarding: (context) => const OnboardingScreen(),
+        AppRoutes.home: (context) => const HomeScreen(),
+      },
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: const Center(
-        child: Text(
-          'Welcome to Rewriter',
-          style: TextStyle(fontSize: 24),
-        ),
-      ),
-    );
-  }
+class AppRoutes {
+  static const String onboarding = '/onboarding';
+  static const String home = '/home';
 }
